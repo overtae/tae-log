944cb335bb2452ed63626490390647a20beb7dc4
taeyoung <rlaxodud911@gmail.com>
2022-10-18 22:17:14 +0900

    :sparkles: 사이트 메타데이터, gatsby 플러그인 설정 수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require(`dotenv`).config()
+require(`dotenv`).config();
 
-const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE;
 
 /**
  * @type {import('gatsby').GatsbyConfig}
@@ -10,13 +10,13 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Tae.log`,
+    siteTitleAlt: `Tae.log - Today I Learned`,
+    siteHeadline: `Tae.log - Today I Learned`,
+    siteUrl: `https://overtae.github.io/tae-log/`,
+    siteDescription: `공부한 것들을 올리는 블로그입니다.`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@overtae`,
   },
   plugins: [
     {
@@ -35,12 +35,8 @@ module.exports = {
         ],
         externalLinks: [
           {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `github`,
+            url: `https://github.com/overtae`,
           },
         ],
       },
@@ -54,9 +50,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Tae.log - Today I Learned`,
+        short_name: `Tae.log`,
+        description: `공부한 것들을 올리는 블로그입니다.`,
         start_url: `/`,
         background_color: `#fff`,
         // This will impact how browsers show your PWA/website
@@ -96,8 +92,8 @@ module.exports = {
           {
             serialize: ({ query: { site, allPost } }) =>
               allPost.nodes.map((post) => {
-                const url = site.siteMetadata.siteUrl + post.slug
-                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+                const url = site.siteMetadata.siteUrl + post.slug;
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`;
 
                 return {
                   title: post.title,
@@ -105,8 +101,8 @@ module.exports = {
                   excerpt: post.excerpt,
                   url,
                   guid: url,
-                  custom_elements: [{ "content:encoded": content }],
-                }
+                  custom_elements: [{ 'content:encoded': content }],
+                };
               }),
             query: `
               {
@@ -121,7 +117,7 @@ module.exports = {
               }
             `,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Tae.log - Today I Learned`,
           },
         ],
       },
@@ -135,4 +131,4 @@ module.exports = {
       },
     },
   ].filter(Boolean),
-}
+};
