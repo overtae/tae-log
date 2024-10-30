cbdde22c0666371e7379a403613809ac25960171
taeyoung <rlaxodud911@gmail.com>
2024-10-30 14:57:01 +0900

    :lipstick: Design: 레이아웃 푸터 컴포넌트 작성

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="flex w-full items-center justify-center px-6 py-12">
+      <p>좋은 하루 되세요 🙌</p>
+    </footer>
+  )
+}
+
+export default Footer
